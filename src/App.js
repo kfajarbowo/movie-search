d83ee5d521110ed38b020ec8c5488643a3284a22
kfajarbowo <bagas@workshop.web.id>
2020-11-01 22:18:08 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchMovies from './components/searchMovies';
+// import Background from './bg.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="body-wrap">
+      <main className="content">
+			<div className="container">
+				<h1 className="title">React Movie Search</h1>
+				<SearchMovies></SearchMovies>
+			</div>
+
+      </main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/searchMovies.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+export default function SearchMovies() {
+
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=ecb4f95925056117fe020a3233ea222e&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try{   
+            const res = await fetch(url);
+            const data = await res.json();
+            setMovies(data.results);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    return (
+        <>
+        <form className="form" onSubmit={searchMovies}>
+            <label className="label" htmlFor="query">Movie Name</label>
+            <input className="input" name="query"
+             type="text" placeholder="i.e Hobbit"
+             value={query} onChange={(e) => setQuery(e.target.value)}
+             />
+            <button className="button" type="submit">Search</button>
+        </form>
+
+        <div className="card-list">
+             {movies.map(movie => movie.title)}
+        </div>
+        </>
+    )
+}
